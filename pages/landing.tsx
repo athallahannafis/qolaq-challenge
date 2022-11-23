874fab3c7d183e7e777f48c76cbb4ccaf88e7155
athallahannafis <athallahannafis@gmail.com>
2022-11-24 01:26:45 +0700

    feat: intro section

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import NavigationBar from "./navbar"
 import { Button } from "react-bootstrap";
 
+import qolaqlearn from "../assets/images/learn-content-1.png";
+import Image from "next/image";
 
 
 
@@ -25,8 +27,19 @@ export default function Landing({}) {
     <div>
         <NavigationBar/>
         <div className="flex flex-col items-center">
-            <p>Landing page</p>
-            <p>Value of toggle: {toggle ? "True" : "False"}</p>
+            <div className="w-full py-10">
+                <div className="md:container md:mx-auto">
+                    <div className="md:flex flex-row-reverse justify-center items-center px-10">
+                        {/* Text and title */}
+                        <Image width={490} height={480}  alt="qolaq-learn" src={qolaqlearn}/>
+                        <div className="flex flex-col">
+                            <p className="text-5xl text-violet-900 font-bold">QolaQ’s Decentralized Mutual Aid Platform</p>
+                            <div className="py-2"/>
+                            <p>A simpler, fairer and easier way to support you and the community.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     )
