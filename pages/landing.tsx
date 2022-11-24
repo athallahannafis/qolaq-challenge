aec7d5c4526451eb878477f0e89fc69aea5c984d
athallahannafis <athallahannafis@gmail.com>
2022-11-24 19:22:41 +0700

    feat: last section

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -10,7 +10,12 @@ import offer2 from "../assets/images/offer-2.png";
 import offer3 from "../assets/images/offer-3.png";
 import offer4 from "../assets/images/offer-4.png";
 import landingContent1 from "../assets/images/landing-content-1.png";
-import landingContent3 from "../assets/images/landing-content-3.png"
+import landingContent3 from "../assets/images/landing-content-3.png";
+import how1 from "../assets/images/how-1.png";
+import how2 from "../assets/images/how-2.png";
+import how3 from "../assets/images/how-3.png";
+import how4 from "../assets/images/how-4.png";
+import Footer from "./footer";
 
 
 export default function Landing({}) {
@@ -40,7 +45,6 @@ export default function Landing({}) {
     <div>
         <NavigationBar/>
         <div className="flex flex-col items-center">
-            
             {/* Intro section */}
             <div className="w-full py-10">
                 <div className="lg:container lg:mx-auto lg:flex flex-row-reverse self-center justify-center items-center px-10 ">
@@ -94,7 +98,7 @@ export default function Landing({}) {
                                 <div className="flex flex-row items-center">
                                     <Image width={54} height={54} alt={"offer-1"} src={offer3}/>
                                     <div className="px-2"/>
-                                    <p className="text-2xl font-bold">Definitely Cheaper</p>
+                                    <p className="text-2xl font-bold">Decentralized Decision</p>
                                 </div>
                                 <div className="py-3"/>
                                 <p>Our “pay-when-needed” model is less costly to the Members, by removing the signficant cost structure — including commissions, distribution and underwriting — of insurance companies.</p>
@@ -104,7 +108,7 @@ export default function Landing({}) {
                                 <div className="flex flex-row items-center">
                                     <Image width={54} height={54} alt={"offer-1"} src={offer2}/>
                                     <div className="px-2"/>
-                                    <p className="text-2xl font-bold">Definitely Cheaper</p>
+                                    <p className="text-2xl font-bold">No Premiums Ever</p>
                                 </div>
                                 <div className="py-3"/>
                                 <p>Our “pay-when-needed” model is less costly to the Members, by removing the signficant cost structure — including commissions, distribution and underwriting — of insurance companies.</p>
@@ -231,11 +235,174 @@ export default function Landing({}) {
                     <div className="lg:w-3/4 flex flex-col items-center">
                         <p className="text-5xl text-center font-bold">How does it work?</p>
                     </div>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10">
+                        <div className="space-y-5 space-x-5">
+                            <div className="w-full flex justify-center">
+                                <Image className="max-w-full" src={how1} alt={"landingContent1"} />
+                            </div>
+                            <div className="text-xl font-bold">
+                                Member
+                            </div>
+                            <p>Complete QolaQ’s registration process to become a Member and hold QLQ Tokens</p>
+                            
+                        </div>
+                        <div className="space-y-5 space-x-5">
+                            <div className="w-full flex justify-center">
+                                <Image className="max-w-full" src={how2} alt={"landingContent1"} />
+                            </div>
+                             <div className="text-xl font-bold">
+                                Voters
+                            </div>
+                            <p>Members can vote to approve or deny submitted claims, also for additional investigation from the Claim Scorer(s)</p>
+                        </div>
+                        <div className="space-y-5 space-x-5">
+                            <div className="w-full flex justify-center">
+                                <Image className="max-w-full" src={how3} alt={"landingContent1"} />
+                            </div>
+                            <div className="text-xl font-bold">
+                                Foundation
+                            </div>
+                            <p>Administers the QolaQ MAP and its related activities, including the Programs and QLQ Tokens</p>
+                        </div>
+                        <div className="space-y-5 space-x-5">
+                            <div className="w-full flex justify-center">
+                                <Image className="max-w-full" src={how4} alt={"landingContent1"} />
+                            </div>
+                            <div className="text-xl font-bold">
+                                Claim Scorer(s)
+                            </div>
+                            <p>Review submitted claims and provide suggestions, including Claim Scores and opinion from the reviews, to be shared with Members before voting</p>
+                        </div>
+                    </div>
+
+                    <div className="grid lg:grid-cols-2 gap-10">
+                        <div>
+                            <p className="text-3xl font-bold">Get Aid</p>
+                            <div className="text-xl font-bold">
+                                1. Register
+                            </div>
+                            <p>Register as a Member by providing information on identity, entry age, and domicile</p>
+                            <div className="text-xl font-bold">
+                                2. Earn
+                            </div>
+                            <p>Members link their digital wallet and acquire QLQ Tokens to participate in MAP</p>
+                            <div className="text-xl font-bold">
+                                3. Select
+                            </div>
+                            <p>Select which MAP Program(s) to join and be assigned a payout</p>
+                            <div className="text-xl font-bold">
+                                4. Submit
+                            </div>
+                            <p>To make a claim, Members submit required documentation (e.g., for Simple Critical Illness Cover, a specialist's diagnosis, test results)</p>
+                        </div>
+
+                        <div>
+                            <p className="text-3xl font-bold">Give Aid</p>
+                            <div className="text-xl font-bold">
+                                5. Review
+                            </div>
+                            <p>Members review each claim case (along with the scores and reviews) and vote if the claim will be paid, denied, or require additional investigation</p>
+                            <div className="text-xl font-bold">
+                                6. Vote
+                            </div>
+                            <p>Members cast their vote to approve/deny the claim based on: Claim's believability and normality of other factors (such as any pre-existing conditions). Members can also vote to request an additional investigation.</p>
+                            <div className="text-xl font-bold">
+                                7. Aid
+                            </div>
+                            <p>Total approved claim payout + costs (admin and direct) contributed equally by all Members and automatically deducted from Members’ digital wallets</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p className="text-5xl text-center font-bold">Voting Process</p>
+                        <p className="text-center">A simple majority vote, in which each Member is eligible to submit one vote</p>
+
+                        <div className="hidden lg:grid gap-10 grid-cols-3 auto-cols-min text-white font-bold">
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    1
+                                </div>
+                                <div className="px-2"/>
+                                Claim cases with claim scores
+                            </div>
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    2
+                                </div>
+                                <div className="px-2"/>
+                                Rank based on criticality
+                            </div>
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    3
+                                </div>
+                                <div className="px-2"/>
+                                Voting round robin
+                            </div>
+                               
+                            
+                            <div className="bg-cyan-700 rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    4
+                                </div>
+                                <div className="px-2"/>
+                                Voters are notified to vote on cases
+                            </div>
+                            <div className="bg-cyan-900 rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    5
+                                </div>
+                                <div className="px-2"/>
+                                Vote is cast
+                            </div>
+                        </div>
+
+                        <div className="lg:hidden grid gap-10 grid-rows-5 text-white font-bold">
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    1
+                                </div>
+                                <div className="px-2"/>
+                                Claim cases with claim scores
+                            </div>
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    2
+                                </div>
+                                <div className="px-2"/>
+                                Rank based on criticality
+                            </div>
+                            <div className="bg-[#008FCF] rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    3
+                                </div>
+                                <div className="px-2"/>
+                                Voting round robin
+                            </div>
+                               
+                            
+                            <div className="bg-cyan-700 rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    4
+                                </div>
+                                <div className="px-2"/>
+                                Voters are notified to vote on cases
+                            </div>
+                            <div className="bg-cyan-900 rounded-2xl p-4 flex flex-row items-center">
+                                <div className="rounded-full bg-white text-black px-3 py-2">
+                                    5
+                                </div>
+                                <div className="px-2"/>
+                                Vote is cast
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
-
             {/* End of how does it work section */}
         </div>
+        <Footer/>
     </div>
     )
 }
